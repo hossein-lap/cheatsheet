973d78dcf033cfacc39a79a3ab55b1b91b3266c8
hossein <hosteam01@gmail.com>
2022-09-05 00:58:13 +0430

    linked README.md files to have preview inside of subdirectories

---
+++ lua/reformat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#  _  _
+# | || | github: hossein-lap
+# | __ | email:  hosteam01@gmail.com
+# |_||_| matrix: hosaidenpwd:matrix.org
+#
+set -e
+
+sed -i "s/’/'/g" lua.md
+sed -i 's/”/"/g' lua.md
